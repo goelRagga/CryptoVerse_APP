45a750ef9a072f812ea76a595dfa15f54cfcfbc5
goelRagga <raghavgoel847@gmail.com>
2021-09-19 15:03:56 +0530

    Layout addded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+              <Homepage />
+            </Route>
+              <Route exact path='/exchanges'>
+                  <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies />
+              </Route>
+              <Route exact path='/cypto/:coinId'>
+                  <CryptoDetails/>
+              </Route>
+              <Route exact path='/news'>
+                  <News/>
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+     <div className="footer">
+       
+     </div>
     </div>
   );
 }
