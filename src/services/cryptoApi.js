8a3fbc9600e094316a7e50654ff15722e039c640
goelRagga <raghavgoel847@gmail.com>
2021-09-21 21:49:29 +0530

    Exchanges added final commit

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,26 +14,21 @@ const createRequest = (url) =>( { url, headers: cryptoApiHeaders } )
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery:fetchBaseQuery({baseUrl}),
-    endpoints:(builder) => ({
-        getCryptos:builder.query({
-            query:(count)=>createRequest(`/coins?limit=${count}`)
-        }),
-         getCryptoDetails: builder.query({
+    endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+    getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
     }),
-    })
-})
+  }),
+});
 
-export const {
-     useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery}=cryptoApi;
-
-
-
-
-
-
-
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
 
